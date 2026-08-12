15d27a9671c40b1e2537255eb65c47b80438415a
Chrisgip1 <95988671+Chrisgip1@users.noreply.github.com>
2026-08-11 22:54:35 -0500

    Change API endpoint from '/api/stamp-cards' to '/api/stampcards'

--- backend/src/test/java/com/flowty/controller/StampCardController.java
+++ backend/src/test/java/com/flowty/controller/StampCardController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/stamp-cards")
+@RequestMapping("/api/stampcards")
 public class StampCardController {
 
     private final StampCardService stampCardService;
